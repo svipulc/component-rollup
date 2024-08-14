b9759b68f71e263f4160d866c3390911dc314545
svipulc <vipul.c@simformsolutions.com>
2024-08-14 09:34:39 +0530

    fix: update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,51 @@
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-import packageJson from "./package.json" assert { type: "json" };
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+// import commonjs from "@rollup/plugin-commonjs";
+// import resolve from "@rollup/plugin-node-resolve";
+// import typescript from "@rollup/plugin-typescript";
+// import autoprefixer from "autoprefixer";
+// import dts from "rollup-plugin-dts";
+// import postcss from "rollup-plugin-postcss";
+// import tailwindcss from "tailwindcss";
+// import packageJson from "./package.json" assert { type: "json" };
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    css: {
-      postcss: {
-        plugins: [tailwindcss, autoprefixer],
-      },
-    },
-    plugins: [
-      resolve({
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        skip: ["react", "react-dom"],
-      }),
-      commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.json",
-        exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.tsx"],
-      }),
-      postcss({ extensions: [".css"], inject: true, extract: false }),
-    ],
-    external: ["react", "react-dom", "react/jsx-runtime"],
-  },
+// export default [
+//   {
+//     input: "src/index.ts",
+//     output: [
+//       {
+//         file: packageJson.main,
+//         format: "cjs",
+//         sourcemap: true,
+//       },
+//       {
+//         file: packageJson.module,
+//         format: "esm",
+//         sourcemap: true,
+//       },
+//     ],
+//     css: {
+//       postcss: {
+//         plugins: [tailwindcss, autoprefixer],
+//       },
+//     },
+//     plugins: [
+//       resolve({
+//         extensions: [".js", ".jsx", ".ts", ".tsx"],
+//         skip: ["react", "react-dom"],
+//       }),
+//       commonjs(),
+//       typescript({
+//         tsconfig: "./tsconfig.json",
+//         exclude: ["**/*.test.tsx", "**/*.test.ts", "**/*.stories.tsx"],
+//       }),
+//       postcss({ extensions: [".css"], inject: true, extract: false }),
+//     ],
+//     external: ["react", "react-dom", "react/jsx-runtime"],
+//   },
 
-  {
-    input: "dist/esm/types/src/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
-];
+//   {
+//     input: "dist/esm/types/src/index.d.ts",
+//     output: [{ file: "dist/index.d.ts", format: "esm" }],
+//     plugins: [dts()],
+//     external: [/\.css$/],
+//   },
+// ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+// <reference types="vitest" />
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: "./src/index.ts", // Specifies the entry point for building the library.
+      name: "vite-react-ts-button", // Sets the name of the generated library.
+      fileName: format => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+    },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
+  },
+  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
 });
